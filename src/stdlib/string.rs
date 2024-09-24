b6cdc4825718bee95d76dd083828e168789e3386
Maarten Staa <maartens@spotify.com>
2024-09-24 12:40:40 +0200

    first implementation of stdlib::string.format

--- src/stdlib/string.rs
+++ src/stdlib/string.rs
@@ -8,6 +8,10 @@ use crate::{
 pub static STRING: LazyLock<LuaValue> = LazyLock::new(|| {
     let mut string = LuaTable::new();
 
+    string.insert(
+        "format".into(),
+        LuaObject::NativeFunction("format", format).into(),
+    );
     string.insert("len".into(), LuaObject::NativeFunction("len", len).into());
     string.insert(
         "reverse".into(),
@@ -41,6 +45,36 @@ macro_rules! require_string {
     ($input:expr, $name:expr) => {
         require_string!($input, $name, 0)
     };
+}
+
+fn format(_: &mut VM, input: Vec<LuaValue>) -> miette::Result<Vec<LuaValue>> {
+    let mut format_string = require_string!(input, "format").clone();
+
+    for (i, v) in input.iter().skip(1).enumerate() {
+        let Some((index, format)) = format_string.iter().enumerate().find_map(|(i, c)| {
+            if *c == b'%' {
+                match format_string.get(i + 1) {
+                    Some(b's') => Some((i, "s")),
+                    Some(b'%') => None,
+                    Some(f) => todo!("format specifier: {:?}", f),
+                    None => None,
+                }
+            } else {
+                None
+            }
+        }) else {
+            break;
+        };
+
+        let formatted_value = match format {
+            "s" => v.to_string(),
+            _ => unreachable!(),
+        };
+
+        format_string.splice(index..index + 2, formatted_value.bytes());
+    }
+
+    Ok(vec![LuaValue::String(format_string)])
 }
 
 fn len(_: &mut VM, input: Vec<LuaValue>) -> miette::Result<Vec<LuaValue>> {
