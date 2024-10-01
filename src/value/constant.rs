95f44548f176275273fd10b314d8e8ad27184d1a
Maarten Staa <maartens@spotify.com>
2024-10-01 11:44:54 +0200

    fix accidental constant overlap
    if a file contained both the constant integer x, and the float x.0, they
    would be collapsed into the same constant, so the second value would be
    wrong

--- src/value/constant.rs
+++ src/value/constant.rs
@@ -8,7 +8,7 @@ pub struct LuaFunctionDefinition {
     pub upvalues: usize,
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone)]
 pub enum LuaConst {
     Marker,
     Nil,
@@ -17,4 +17,25 @@ pub enum LuaConst {
     String(Vec<u8>),
     Function(LuaFunctionDefinition),
     Table(LuaTable),
+}
+
+impl PartialEq for LuaConst {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (LuaConst::Marker, LuaConst::Marker) => true,
+            (LuaConst::Nil, LuaConst::Nil) => true,
+            (LuaConst::Boolean(a), LuaConst::Boolean(b)) => a == b,
+            (LuaConst::Number(a), LuaConst::Number(b)) => match (a, b) {
+                (LuaNumber::Integer(a), LuaNumber::Integer(b)) => a == b,
+                (LuaNumber::Float(a), LuaNumber::Float(b)) => a == b,
+                // NOTE: While the values may be equal, these are distinct constants!
+                (LuaNumber::Integer(_), LuaNumber::Float(_)) => false,
+                (LuaNumber::Float(_), LuaNumber::Integer(_)) => false,
+            },
+            (LuaConst::String(a), LuaConst::String(b)) => a == b,
+            (LuaConst::Function(a), LuaConst::Function(b)) => a == b,
+            (LuaConst::Table(a), LuaConst::Table(b)) => a == b,
+            _ => false,
+        }
+    }
 }
