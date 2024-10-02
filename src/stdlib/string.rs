20b5868f459fe31cf5c95bdd47c21b3dad26c6c3
Maarten Staa <maartens@spotify.com>
2024-10-02 09:44:21 +0200

    implement pattern handling in stdlib::string
    compiles patterns down to regexes under the hood

--- src/stdlib/string.rs
+++ src/stdlib/string.rs
@@ -1,4 +1,6 @@
+use regex::bytes::Regex;
 use std::{cmp::Ordering, sync::LazyLock};
+use subslice::SubsliceExt;
 
 use crate::{
     macros::{get_string, require_number, require_string},
@@ -18,6 +20,10 @@ pub static STRING: LazyLock<LuaValue> = LazyLock::new(|| {
         LuaObject::NativeFunction("format", format).into(),
     );
     string.insert("len".into(), LuaObject::NativeFunction("len", len).into());
+    string.insert(
+        "match".into(),
+        LuaObject::NativeFunction("match", r#match).into(),
+    );
     string.insert("rep".into(), LuaObject::NativeFunction("rep", rep).into());
     string.insert(
         "reverse".into(),
@@ -27,6 +33,140 @@ pub static STRING: LazyLock<LuaValue> = LazyLock::new(|| {
 
     string.into()
 });
+
+#[derive(Debug)]
+enum LuaPattern {
+    Plain(Vec<u8>),
+    Regex(Regex),
+}
+
+fn pattern_to_regex(pattern: &[u8]) -> miette::Result<LuaPattern> {
+    let mut regex = String::with_capacity(pattern.len());
+    let len = pattern.len();
+    let mut chars = pattern.iter().enumerate().peekable();
+    let mut any_special = false;
+    let mut inside_character_class = false;
+    while let Some((i, char)) = chars.next() {
+        match char {
+            b'%' => {
+                any_special = true;
+
+                if i == len - 1 {
+                    return Err(miette::miette!("malformed pattern (ends with '%')"));
+                }
+
+                match chars.next().unwrap().1 {
+                    // Character classes
+                    b'a' => regex.push_str("[a-zA-Z]"),
+                    b'A' => regex.push_str("[^a-zA-Z]"),
+                    b'c' => regex.push_str("[\x00-\x1F]"),
+                    b'C' => regex.push_str("[^\x00-\x1F]"),
+                    b'd' => regex.push_str("\\d"),
+                    b'D' => regex.push_str("\\D"),
+                    b'g' => regex.push_str("[\x21-\x7E]"),
+                    b'G' => regex.push_str("[^\x21-\x7E]"),
+                    b'l' => regex.push_str("[a-z]"),
+                    b'L' => regex.push_str("[^a-z]"),
+                    b'p' => regex.push_str("[[:punct:]]"),
+                    b'P' => regex.push_str("[^[:punct:]]"),
+                    b's' => regex.push_str("\\s"),
+                    b'S' => regex.push_str("\\S"),
+                    b'u' => regex.push_str("[A-Z]"),
+                    b'U' => regex.push_str("[^A-Z]"),
+                    b'w' => regex.push_str("\\w"),
+                    b'W' => regex.push_str("\\W"),
+                    b'x' => regex.push_str("[0-9a-fA-F]"),
+                    b'X' => regex.push_str("[^0-9a-fA-F]"),
+
+                    // Backreferences
+                    num @ b'0'..=b'9' => {
+                        regex.push('\\');
+                        regex.push(*num as char);
+                    }
+
+                    // Others
+                    b'b' => return Err(miette::miette!("unsupported pattern character: '%b'")),
+                    b'f' => return Err(miette::miette!("unsupported pattern character: '%f'")),
+
+                    // Things we have to escape for regex
+                    other @ b'%'
+                    | other @ b'.'
+                    | other @ b'['
+                    | other @ b']'
+                    | other @ b'*'
+                    | other @ b'+'
+                    | other @ b'-'
+                    | other @ b'('
+                    | other @ b')'
+                    | other @ b'?'
+                    | other @ b'^'
+                    | other @ b'$' => {
+                        regex.push('\\');
+                        regex.push(*other as char)
+                    }
+
+                    other => regex.push(*other as char),
+                }
+            }
+
+            // Anchors
+            b'^' if i == 0 => {
+                any_special = true;
+                regex.push('^')
+            }
+            b'$' if i == len - 1 => {
+                any_special = true;
+                regex.push('$')
+            }
+            b'^' if !inside_character_class => {
+                any_special = true;
+                regex.push_str("\\^")
+            }
+            b'$' => {
+                any_special = true;
+                regex.push_str("\\$")
+            }
+
+            // Translate special characters
+            b'-' if !inside_character_class => {
+                any_special = true;
+                regex.push_str("*?")
+            }
+
+            // Special to regex, but not special in Lua
+            b'\\' | b'{' | b'}' => {
+                regex.push('\\');
+                regex.push(*char as char)
+            }
+
+            // Some don't need translating, but we just mark them as special
+            b'^' | b'(' | b')' | b'.' | b'*' | b'+' | b'?' => {
+                any_special = true;
+                regex.push(*char as char)
+            }
+            b'[' => {
+                any_special = true;
+                regex.push(*char as char);
+                inside_character_class = true;
+            }
+            b']' => {
+                any_special = true;
+                regex.push(*char as char);
+                inside_character_class = false;
+            }
+
+            other => regex.push(*other as char),
+        }
+    }
+
+    if any_special {
+        Regex::new(&regex)
+            .map(LuaPattern::Regex)
+            .map_err(|e| miette::miette!(e))
+    } else {
+        Ok(LuaPattern::Plain(pattern.to_vec()))
+    }
+}
 
 fn find(_: &mut VM, input: Vec<LuaValue>) -> miette::Result<Vec<LuaValue>> {
     let s = require_string!(input, "find");
@@ -60,16 +200,19 @@ fn find(_: &mut VM, input: Vec<LuaValue>) -> miette::Result<Vec<LuaValue>> {
         Ordering::Equal => unreachable!(),
     };
 
-    // TODO: Handle actual patterns
+    let pattern = pattern_to_regex(pattern)?;
+    let start_end = match &pattern {
+        LuaPattern::Plain(pattern) => s
+            .find(pattern)
+            .map(|start| (start as i64, start as i64 + pattern.len() as i64)),
+        LuaPattern::Regex(regex) => regex.find(s).map(|m| (m.start() as i64, m.end() as i64)),
+    };
 
-    let start = s.windows(pattern.len()).position(|w| w == pattern);
-    Ok(match start {
-        Some(start) => {
+    Ok(match start_end {
+        Some((start, end)) => {
             vec![
-                LuaValue::Number(LuaNumber::Integer(start as i64 + init)),
-                LuaValue::Number(LuaNumber::Integer(
-                    start as i64 + init - 1 + pattern.len() as i64,
-                )),
+                LuaValue::Number(LuaNumber::Integer(start + init)),
+                LuaValue::Number(LuaNumber::Integer(end + init - 1)),
             ]
         }
         None => vec![LuaValue::Nil],
@@ -128,6 +271,36 @@ fn len(_: &mut VM, input: Vec<LuaValue>) -> miette::Result<Vec<LuaValue>> {
     Ok(vec![LuaValue::Number(LuaNumber::Integer(
         input.len() as i64
     ))])
+}
+
+fn r#match(_: &mut VM, mut input: Vec<LuaValue>) -> miette::Result<Vec<LuaValue>> {
+    let s = require_string!(input, "match");
+    let pattern = require_string!(input, "match", 1);
+    // TODO: init parameter
+
+    let pattern = pattern_to_regex(pattern)?;
+    match pattern {
+        LuaPattern::Plain(pattern) => match s.find(&pattern) {
+            Some(start) => Ok(vec![LuaValue::String(
+                s[start..start + pattern.len()].to_vec(),
+            )]),
+            None => Ok(vec![LuaValue::Nil]),
+        },
+        LuaPattern::Regex(regex) => {
+            let captures = regex.captures(s);
+
+            match captures {
+                Some(captures) => {
+                    if let Some(capture) = captures.get(0) {
+                        Ok(vec![LuaValue::String(capture.as_bytes().to_vec())])
+                    } else {
+                        Ok(vec![input.swap_remove(0)])
+                    }
+                }
+                None => Ok(vec![LuaValue::Nil]),
+            }
+        }
+    }
 }
 
 fn rep(_: &mut VM, input: Vec<LuaValue>) -> miette::Result<Vec<LuaValue>> {
